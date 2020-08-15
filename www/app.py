b40b9ad8b9562f4c5abb7af018a707b9dc5f39db
Wulong Cha <ziyean@outlook.com>
2020-08-15 17:43:44 +0800

    add orm.py, add some comments on add.py

--- www/app.py
+++ www/app.py
@@ -6,6 +6,43 @@ from datetime import datetime
 from aiohttp import web
 
 
+
+HOST = '127.0.0.1'
+PORT = 9000
+
+routes = web.RouteTableDef()
+
+# Handling request to app by routers in a decorator way
+# Router: used for dispatching URLs to handler HTTP method + path
+@routes.get('/')
+async def handler(request):
+    # handler: a coroutine
+    # take Request instance as its only param
+    # return a Response instance
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+
+
+def init():
+    app = web.Application()
+    app.add_routes(routes)
+    logging.info('Server started at http://{}:{}'.format(HOST, PORT))
+    web.run_app(app, host=HOST, port=PORT)
+
+init()
+
+# Another way of handling request
+
+# async def handler(request):
+#     ...
+
+# def init():
+#     app = web.Application()
+#     app.add_routes([web.get('/', handler)])
+#     web.run_app(app, host=HOST, port=PORT)
+
+
+# The depreciated method:
+
 # def index(request):
 #     # 处理url
 #     return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
@@ -21,24 +58,6 @@ from aiohttp import web
 # loop = asyncio.get_event_loop()
 # loop.run_until_complete(init(loop))
 # loop.run_forever()
-
-HOST = '127.0.0.1'
-PORT = 9000
-
-routes = web.RouteTableDef()
-
-@routes.get('/')
-async def handler(request):
-    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
-
-
-def init():
-    app = web.Application()
-    app.add_routes(routes)
-    logging.info('Server started at http://{}:{}'.format(HOST, PORT))
-    web.run_app(app, host=HOST, port=PORT)
-
-init()
 
 
 
