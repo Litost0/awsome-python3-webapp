b20942093e6751ff8e24a443f7ec95450219119c
Wulong Cha <ziyean@outlook.com>
2020-09-21 13:23:43 +0800

    migrate to laptop

--- www/orm.py
+++ www/orm.py
@@ -27,7 +27,8 @@ async def select(sql, args, size=None):
     log(sql, args)
     global __pool
     async with __pool.acquire() as conn: # __pool.get() in Liao's code
-        async with conn.cursor(aiomysql.DictCursor) as cur:
+        async with conn.cursor(aiomysql.DictCursor) as cur: # returns results as dictionary
+            # SQL占位符：？，MySQL占位符： %s
             await cur.execute(sql.replace('?', '%s'), args or ())
             if size:
                 rs = await cur.fetchmany(size)
@@ -44,7 +45,7 @@ async def execute(sql, args, autocommit=True):
             await conn.begin()
         try:
             async with conn.cursor(aiomysql.DictCursor) as cur:
-                await cur.execute(sql.release('?', '%s'), args)
+                await cur.execute(sql.replace('?', '%s'), args)
                 affected = cur.rowcount
             if not autocommit:
                 await conn.commit()
@@ -68,7 +69,7 @@ class Field(object):
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
         self.column_type = column_type
-        self.primary_key = primary_key
+        self.primary_key = primary_key # boolean
         self.default = default
 
     def __str__(self):
@@ -83,22 +84,26 @@ class StringField(Field):
 
 class BooleanField(Field):
 
-    pass
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
 
 
 class IntegerField(Field):
 
-    pass
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
 
 
 class FloatField(Field):
 
-    pass
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
 
 
 class TextField(Field):
 
-    pass
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
 
 
 class ModelMetaclass(type):
@@ -112,17 +117,32 @@ class ModelMetaclass(type):
         fields = []
         primaryKey = None
 
-        pass # ...
-       
-        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info(' found mapping: %s ===> %s' % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    if primary_key:
+                        raise StandardError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else: 
+                    fields.append(k)
+        if not primaryKey:
+            raise StandardError('Primary key not found.')
+
+        if k in mappings.keys():
+            attrs.pop(k)
+
+        escaped_fields = [lambda s: '`{}`'.format(s) for s in fields]
+        
 
         attrs['__mappings__'] = mappings 
         attrs['__table__'] = tableName
-
-        
-        pass # ...
-
-        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__primary_key__'] = primaryKey
+        attrs['__fields__'] = fields # 主键以外的属性名
+        attrs['__select__'] = 'SELECT `%s`, %s FROM `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'INSERT INTO `%s` (%s, `%s`) VALUES (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
 
         
         pass # ...
@@ -146,7 +166,43 @@ class Model(dict, metaclass=ModelMetaclass):
 
     def getValueOrDefault(self, key):
         pass
-        
+
+    @classmethod
+    async def findAll(cls, where=None, args=None, **kw):
+        # find objects by where clause.
+        sql = [cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args = []
+        orderBy = kw.get('orderBy', None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+        limit = kw.get('limit', None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit, int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append('?, ?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value: %s' % str(limit))
+        rs = await select(' '.join(sql), args)
+        return [cls(**r) for r in rs]
+
+
+    pass
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failded to insert record: affected rows: %s' % rows)
 
 
 
